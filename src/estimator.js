8e2108497b91a06ee0af5d11219c9fcb2ec86dca
Fatiu Isiaka <fatiuisiaka@MacBook-Pro.local>
2020-04-11 22:13:38 +0100

    done with ch1-3

--- src/estimator.js
+++ src/estimator.js
@@ -1,8 +1,20 @@
 const covid19ImpactEstimator = (data) => ({
-  data,
+  data: {
+    region: {
+      name: 'Africa',
+      avgAge: 19.7,
+      avgDailyIncomeInUSD: 5,
+      avgDailyIncomePopulation: 0.71
+    },
+    periodType: 'days',
+    timeToElapse: 58,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
+  },
   impact: {
     currentlyInfected: reportedCases * 10,
-    infectionsByRequestedTime: currentlyInfected * (Math.pow(2, (Math.floor(timeToElapse / 3)))),
+    infectionsByRequestedTime: currentlyInfected * (2 ** (Math.floor(timeToElapse / 3))),
     severeCasesByRequestedTime: 0.15 * infectionsByRequestedTime,
     hospitalBedsByRequestedTime: totalHospitalBeds * 0.35,
     casesForICUByRequestedTime: infectionsByRequestedTime * 0.5,
@@ -12,7 +24,7 @@ const covid19ImpactEstimator = (data) => ({
   },
   severeImpact: {
     currentlyInfected: reportedCases * 50,
-    infectionsByRequestedTime: currentlyInfected * (Math.pow(2, (Math.floor(timeToElapse / 3)))),
+    infectionsByRequestedTime: currentlyInfected * (2 ** (Math.floor(timeToElapse / 3))),
     severeCasesByRequestedTime: 0.15 * infectionsByRequestedTime,
     hospitalBedsByRequestedTime: totalHospitalBeds * 0.35,
     casesForICUByRequestedTime: infectionsByRequestedTime * 0.5,
