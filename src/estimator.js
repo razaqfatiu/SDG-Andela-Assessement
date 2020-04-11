34a0448755446ffb2625522796268a2f0c9be51b
Fatiu Isiaka <fatiuisiaka@MacBook-Pro.local>
2020-04-11 21:57:42 +0100

    done with ch1-3

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,15 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => ({
+  data,
+  impact: {
+    currentlyInfected: reportedCases * 10,
+    infectionsByRequestedTime: currentlyInfected * (Math.pow(2, (Math.floor(days / 3))))
+
+  },
+  severeImpact: {
+    currentlyInfected: reportedCases * 50,
+    infectionsByRequestedTime: currentlyInfected * (Math.pow(2, (Math.floor(days / 3))))
+
+  }
+});
 
 export default covid19ImpactEstimator;
